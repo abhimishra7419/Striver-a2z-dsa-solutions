7ea0029a594eaf94ee08330618f099eaa60cc738
ABHI <abhimishra7419@gmail.com>
2026-08-06 23:20:37 +0530

    6 Aug 2026 work

--- 06_Linked list/3 Medium Problems of LL/1 Find middle element in a Linked List.py
+++ 06_Linked list/3 Medium Problems of LL/1 Find middle element in a Linked List.py
@@ -1,33 +1,39 @@
 '''My approach'''
+# class Node:
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+# class Solution:
+#     def lenth(self, head):
+#         len = 0
+#         current = head
+#         while current:
+#             len += 1
+#             current = current.next
+#         return len
+#     def middle(self, head, len):
+#         if len == 0:
+#             return -1
+#         mid = len//2
+#         current = head
+#         for _ in range(mid):
+#             current = current.next
+#         return current.data
+# if __name__ == "__main__":
+#     head = Node(0)
+#     head.next = Node(1)
+#     head.next.next = Node(2)
+#     head.next.next.next = Node(3)
+#     head.next.next.next.next = Node(4)
+#     head.next.next.next.next.next = Node(5)
+#     a = Solution()
+#     len = a.lenth(head)
+#     print(a.middle(head, len))
+
+
+'''Brute force'''
 class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
-
-class Solution:
-    def lenth(self, head):
-        len = 0
-        current = head
-        while current:
-            len += 1
-            current = current.next
-        return len
-    def middle(self, head, len):
-        if len == 0:
-            return -1
-        mid = len//2
-        current = head
-        for _ in range(mid):
-            current = current.next
-        return current.data
-if __name__ == "__main__":
-    head = Node(0)
-    head.next = Node(1)
-    head.next.next = Node(2)
-    head.next.next.next = Node(3)
-    head.next.next.next.next = Node(4)
-    head.next.next.next.next.next = Node(5)
-
-    a = Solution()
-    len = a.lenth(head)
-    print(a.middle(head, len))
+    
